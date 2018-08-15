2b4a7d592cc2f1fb5bc9a4054b39ebb8d74764ba
Tre <tretaylorbrady@gmail.com>
2018-08-15 11:21:14 -0600

    i did this

--- app/src/main/java/com/tylerbrady34gmail/fitness/Workout.java
+++ app/src/main/java/com/tylerbrady34gmail/fitness/Workout.java
@@ -1,12 +1,33 @@
 package com.tylerbrady34gmail.fitness;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Workout {
+
     private List<IExercise> exerciseList;
     //date//
 
+    public Workout(List<IExercise> exerciseList) {
+        this.exerciseList = exerciseList;
+    }
+
+    public Workout() {
+        exerciseList = new ArrayList<>();
+    }
+
+
     public List<IExercise> getExercise(){
         return exerciseList;
+    }
+
+    public void addExercise(List<IExercise> list){
+        for(int i = 0; i < list.size(); i++){
+            exerciseList.add(list.get(i));
+        }
+    }
+
+    public void addExercise(IExercise exercise) {
+        exerciseList.add(exercise);
     }
 }
